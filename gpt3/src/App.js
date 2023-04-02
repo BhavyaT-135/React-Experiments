585837f7f32a95a974dd00bb2cab8e1536594626
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-02 16:10:56 +0530

    ✨ Exporting containers as default

--- gpt3/src/App.js
+++ gpt3/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import {Article, Brand, CTA, Feature, Navbar} from './components'
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
 
 const App = () => {
   return (
