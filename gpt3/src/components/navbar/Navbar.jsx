7216757b457dfdf035e15fe6d6c0aea263facc2b
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-04 13:34:24 +0530

    ✨ Navbar Links Added

--- gpt3/src/components/navbar/Navbar.jsx
+++ gpt3/src/components/navbar/Navbar.jsx
@@ -9,8 +9,15 @@ const Navbar = () => {
   return (
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
-        <div className='gpt3__navbar-links__logo'>
+        <div className='gpt3__navbar-links_logo'>
           <img src={logo}  alt='Logo' />
+        </div>
+        <div className='gpt3__navbar-links_container'>
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#wgpt3'>What is GPT3?</a></p>
+          <p><a href='#possibility'>Open AI</a></p>
+          <p><a href='#features'>Case Studies</a></p>
+          <p><a href='#blog'>Library</a></p>
         </div>
       </div>
     </div>
