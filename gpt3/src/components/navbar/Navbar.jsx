ca04a7838aec9918ae79c4b5fd4c2e6c8a23d56d
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-04 13:20:05 +0530

    🍱 Added asset images

--- gpt3/src/components/navbar/Navbar.jsx
+++ gpt3/src/components/navbar/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './navbar.css'
+import { RiMenu3Line, RiCloseLine} from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
+
+// CSS Naming Convention BEM -> Block Element Modifier
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links__logo'>
+          <img src={logo}  alt='Logo' />
+        </div>
+      </div>
+    </div>
   )
 }
 
